8032d9b7b06984ade0c3a159f43d6ed2028121e7
larryoatmeal <larryoatmeal@gmail.com>
2015-12-27 19:37:33 -0800

    pretty shapes

--- demo.py
+++ demo.py
@@ -1,12 +1,14 @@
-import sys, pygame
+import sys, pygame, math
 pygame.init()
 
 # Colors
 BLACK = (0 , 0, 0 )
 WHITE = (255, 255, 255)
 LAVENDAR = (150, 0, 150)
-# 3:2 aspect ratio. 30x20 16x16 blocks
+RED = (255, 0, 0)
+# 3:2 aspect ratio. 30x20 UNITxUNIT blocks
 size = (480, 320)
+UNIT = 16
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("NEXT 3E")
 
@@ -14,20 +16,39 @@ pygame.display.set_caption("NEXT 3E")
 keepGoing = True
 clock = pygame.time.Clock()
 
-# while keepGoing:
-# 	#Poll events
-# 	for event in pygame.event.get():
-# 		if event.type == pygame.QUIT:
-# 			keepGoing = False
+# Fonts (font, size, bold, italics) Not working right now
+# font = pygame.font.SysFont('Calibri', 25, True, False)
+# Render text (text, anti-aliasing, color)
+# text = font.render("6.3E", True, BLACK)
 
 while keepGoing:
+	# Poll for events
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			keepGoing = False;
+		elif event.type == pygame.KEYDOWN:
+			# handle keydown
+			print("KEYDOWN")
+		elif event.type == pygame.KEYUP:
+			# handle keyup
+			print("KEYUP")
+	# Update world
 
-	#Update world
-	#Draw world
-
+	# Draw world
 	screen.fill(LAVENDAR) #need to clear screen on each Draw
-	pygame.display.flip() #double buffer
+
+	pygame.draw.rect(screen, RED, [UNIT*3, UNIT*3, UNIT*5, UNIT*5])
+	pygame.draw.line(screen, RED, [0, 0], [UNIT*29, UNIT*19], 5)
+	#Draw sinusoid
+	for i in range(2 * UNIT, 25 * UNIT):
+		x = i
+		y = int(3*UNIT*math.cos(x/(4*UNIT))) + 10*UNIT
+		pygame.draw.line(screen, RED, [x, y], [x + 1, y])
+	
+	# Draw text
+	# screen.blit(text, [0, 0])
+
+	# Display
+	pygame.display.flip() 
 	clock.tick(60) #60 fps
+pygame.quit()
